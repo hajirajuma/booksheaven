263ca050023f20e22e722782d6f35f75cefe79c0
hajirajuma <hajiraj475@gmail.com>
2025-07-24 16:40:09 -0700

    adding style

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import images from "next/image";
 import data1 from "../public/books/data1.jpg";
 import Image from "next/image";
-import { Search } from "lucide-react";
+import { MoveRight, Search } from "lucide-react";
 export default function Home() {
   return (
     <>
@@ -17,11 +17,11 @@ export default function Home() {
         <div className="flex justify-center items-center mb-20 ">
           <div className="relative">
         <input type="text" placeholder="search books..." className="w-3xl p-2 border border-yellow-700 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-        <button className="absolute right-2 top-1/2 -translate-y-1/2 bg-blue-700"><Search size={20}/></button>
+        <button className="absolute right-2 top-1/2 -translate-y-1/2 text-yellow-800 "><Search size={20}/></button>
         </div>
         </div>
-        <div className="grid grid-cols-3 grid-rows-2 gap-4">
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+        <div className="grid grid-cols-3 grid-rows-2 gap-4 px-40">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/data.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -30,7 +30,7 @@ export default function Home() {
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
 
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/javascript.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -39,7 +39,7 @@ export default function Home() {
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
 
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/linear.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -48,7 +48,7 @@ export default function Home() {
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
 
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/data.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -57,7 +57,7 @@ export default function Home() {
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
 
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/javascript.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -66,7 +66,7 @@ export default function Home() {
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
 
-          <div className="px-15 top-1 p-4 bg-blue-500 rounded-lg">
+          <div className="items-center py-10 w-64 h-58 top-1 p-4 bg-blue-500 border rounded-md">
             <a href="/books/linear.pdf" target="_blank" rel="noopener noreferrer" className="block">
             <Image src={data1} alt="data1" width={94} height={94} className="rounded-lg transition-transform duration-300 hover:scale-105 hover:shadow-xl"/>
             </a>
@@ -74,6 +74,10 @@ export default function Home() {
             <h2 className="text-black text-xs" >Author: Gordon S.Linoff</h2>
             <p className="text-black text-xs" >publisher: Wiley publishing</p>
           </div>
+          
+            <button className=" hover:scale-105 hover:shadow-xl hover:bg-blue-700  bg-yellow-700 items-center rounded-md text-white p-2 w-40 ml-40 mt-5 shadow-md">Add to Cart</button>
+            <button className=" hover:scale-105 hover:shadow-xl hover:bg-blue-700 bg-yellow-700 rounded-md items-center  text-white p-2  w-40 ml-40 mt-5 shadow-md inline-flex ">Category  <MoveRight size={24} className="text-white ml-10"/></button>
+      
         </div>
 
    </div>
