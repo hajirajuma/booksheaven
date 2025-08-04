616c00eaca175f1d716d2ddc62c824f036bacc58
hajirajuma <hajiraj475@gmail.com>
2025-08-03 23:14:43 -0700

    correcting some error

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -67,7 +67,7 @@ export default function CategoriesPage () {
       {/* Category Buttons at the top */}
       <div className="shadow-sm border-b items-center">
         <div className="max-w-7xl ml-55 px-4 sm:px-6 lg:px-8 py-4 ">
-          <ul className="flex items-center gap-20 flex-wrap ml-36 ">
+          <ul className="flex items-center gap-20 flex-wrap  ">
             <li><Button variant="outline">Education</Button></li>
             <li><Button variant="outline">Romance</Button></li>
             <li><Button variant="outline">History</Button></li>
